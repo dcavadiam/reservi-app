2731601d31207958c798a805d10e4acbd24fb9b1
Diego <dcavadia@uninorte.edu.co>
2025-02-08 00:29:02 -0500

    🔧Fix: dashboard responsive

--- src/containers/dashboard/header/index.tsx
+++ src/containers/dashboard/header/index.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header className="flex items-center justify-between py-4 px-6 border-b-[1px] border-opacity-20 border-white">
-            <h1 className="text-xl font-bold">Reservas</h1>
+            <h1 className="text-xl font-bold text-black dark:text-white">Reservas</h1>
         </header>
     )
 }
--- src/containers/dashboard/main/index.tsx
+++ src/containers/dashboard/main/index.tsx
@@ -24,26 +24,24 @@ import { USERS } from "@/mocks/users";
 export const Main = () => {
     // const data = await getData()
     return (
-        <main className="w-full flex gap-4 p-4 ">
-            <section className="w-1/5 flex gap-4 justify-center">
-                <div className="h-full flex flex-col justify-between items-center pt-7">
+        <main className="w-full flex max-md:flex-col gap-4 p-4 ">
+            <section className="flex gap-4 justify-center md:w-1/5 max-md:flex-col-reverse">
+                <div className="h-full flex md:flex-col justify-between items-center md:pt-7">
                     <span className="text-center text-xs">00:00</span>
                     <span className="text-center text-xs">12:00</span>
                     <span className="text-center text-xs">24:00</span>
                 </div>
                 <div className=" h-full flex flex-col items-center gap-4">
                     <span className="text-center text-xs">Linea de tiempos</span>
-                    <div className="w-20 h-full border dark:border-neutral-800 border-gray-800 rounded-md p-3">
-
+                    <div className="w-full h-14 border dark:border-neutral-800 border-gray-800 rounded-md p-3 md:w-20 md:h-full">
                     </div>
-
                 </div>
             </section>
-            <section className="w-3/5">
+            <section className="md:w-3/5">
                 {/* <UsersTable /> */}
                 <DataTable columns={columns} data={USERS} />
             </section>
-            <section className="w-1/5">
+            <section className="md:w-1/5">
                 <h2 className="text-center font-bold">Nuevo usuario</h2>
                 <UserForm />
                 <h2 className="text-center font-bold">Crear bloque de tiempo</h2>
