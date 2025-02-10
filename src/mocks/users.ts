50695f0ed0dd65d17ad556b55f499526fd738fee
Diego <dcavadia@uninorte.edu.co>
2025-02-09 21:58:59 -0500

    🔧Fix: mock error

--- src/mocks/users.ts
+++ src/mocks/users.ts
@@ -1,20 +1,20 @@
 import { User } from "@/types/user";
 
-export const USERS:User[] = [
-    {
-        id: 1,
-        name: "Jose",
-        email: "jose@user.com",
-        phone: "222 222",
-        city: "Bogotá D.C.",
-        date: "2023-01-01T00:00:00.000Z",
-    },
-    {
-        id: 2,
-        name: "Maria",
-        email: "maria@user.com",
-        phone: "333 333",
-        city: "Cali",
-        date: "2023-01-02T00:00:00.000Z",
-    },
-]
+export const USERS: User[] = [
+  {
+    id: "1",
+    name: "Jose",
+    email: "jose@user.com",
+    phone: "222 222",
+    city: "Bogotá D.C.",
+    date: [],
+  },
+  {
+    id: "2",
+    name: "Maria",
+    email: "maria@user.com",
+    phone: "333 333",
+    city: "Cali",
+    date: [],
+  },
+];
